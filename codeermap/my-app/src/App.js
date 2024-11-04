02db0b33aa6e191c2321bba2ad7f65b2aa46b654
BrigitteHx <brigitteheijkoop@gmail.com>
2024-11-04 16:37:00 +0100

    depot project - feat: added CSS to information page

--- codeermap/my-app/src/App.js
+++ codeermap/my-app/src/App.js
@@ -11,6 +11,7 @@ import Homepage from './components/homepage/homepage';
 import PrivateRoute from './components/PrivateRoute'; // Import the PrivateRoute component
 import { AuthProvider } from './components/AuthContext'; // Import AuthProvider
 import ContactPage from './components/contact/ContactPage'; // Importeer ContactPage component
+import InformationPage from './components/information/InformationPage';
 import './App.css';
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
             <Route path="/password_reset" element={<PasswordReset />} />
             <Route path="/reset/:token" element={<ResetPassword />} />
             <Route path="/contact" element={<ContactPage />} />
+            <Route path="/information" element={<InformationPage />} />
             
             {/* Protect the /home route */}
             <Route 
--- codeermap/my-app/src/components/information/InformationPage.js
+++ codeermap/my-app/src/components/information/InformationPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style/InformationPage.css';
+import './style/InfoForm.css';
 
 const InformationPage = () => {
     return (
